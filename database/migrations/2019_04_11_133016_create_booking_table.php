429d44d106919fe7bbea22179b6cdb7c833c08d8
3w academy <oudaylazaar@gmail.com>
2019-04-11 15:19:38 +0100

    first commit

---
+++ database/migrations/2019_04_11_133016_create_booking_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBookingTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('booking', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('car_id')->unsigned();
+            $table->foreign('car_id')->references('id')->on('cars');
+            $table->bigInteger('customer_id')->unsigned();
+            $table->foreign('customer_id')->references('id')->on('customers');
+            $table->date('date_out');
+            $table->integer('days');
+            $table->boolean('statut');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('booking');
+    }
+}
